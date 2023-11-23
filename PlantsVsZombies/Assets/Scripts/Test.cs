3a8409d81fa2146fa1a9267cfdc3ac5fd7eefdb8
Benjamin Suljevic <bsuljevic1809@gmail.com>
2023-11-23 16:08:25 +0100

    New Folders

---
+++ PlantsVsZombies/Assets/Scripts/Test.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public AnimatorController testAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //testAnimator = this.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
