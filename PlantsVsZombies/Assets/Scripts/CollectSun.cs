4965bdb4239e4688a008233e18f2cf630829f658
Benjamin Suljevic <bsuljevic1809@gmail.com>
2023-11-27 16:18:59 +0100

    new packs added

--- PlantsVsZombies/Assets/Scripts/CollectSun.cs
+++ PlantsVsZombies/Assets/Scripts/CollectSun.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
+
 
 public class CollectSun : MonoBehaviour
 {
     private int score = 0;
+    public Text halloText;
+
     // Start is called before the first frame update
     void Start()
+    {
+        
+    }
+
+    public void AddSun()
     {
         
     }
@@ -14,6 +23,9 @@ public class CollectSun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            halloText.text = "Hallo Welt";
+        }
     }
 }
